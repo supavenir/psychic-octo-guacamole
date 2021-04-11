71358a5e53516dafd3bada24f981a205b49067a1
jcheron <myaddressmail@gmail.com>
2021-04-11 14:32:51 +0200

    add prepared queries

--- fw/queries-old.php
+++ fw/queries-old.php
@@ -9,7 +9,6 @@ function connect($dsn,$user,$password){
 	try {
 		$db = new \PDO('pgsql:host=127.0.0.1;dbname=pgsqllarge', $user, $password);
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-		$db->setAttribute(\PDO::ATTR_PERSISTENT, true);
 
 	} catch (\PDOException $e) {
 		print "Error!: " . $e->getMessage() . "<br/>";
@@ -23,33 +22,24 @@ function query(\PDO $db,string $sql){
 	return $st->fetchAll();
 }
 
-function prepare(\PDO $db,$sql):\PDOStatement{
-	return $db->prepare($sql);
-}
-
 function executeQuery(\PDO $db,string $sql){
-	$st=prepare($db,$sql);
-	return $st->execute();
+	return $db->exec($sql);
 }
 
 function selectPkQuery(\PDO $db,int $max){
-	$st=prepare($db,'select * from user_ where id= ?');
 	$id=\mt_rand(1, $max);
-	return $st->execute([$id]);
+	return query($db,"select * from user_ where id='$id'");
 }
 
 function selectNoPKQuery(\PDO $db){
-	$st=prepare($db,'select * from user_ where firstname= ?');
-	return $st->execute(['Winter']);
+	return query($db,"select * from user_ where firstname='Winter'");
 }
 
 function joinQuery(\PDO $db){
-	$st=prepare($db,'select category_.* from user_ inner join category_ on user_.idCategory=category_.id where firstname= ?');
-	return $st->execute(['Winter']);
+	return query($db,"select category_.* from user_ inner join category_ on user_.idCategory=category_.id where firstname='Winter'");
 }
 
 function updateQuery(\PDO $db,int $max){
-	$st=prepare($db,'update user_ set lastname= ? where id= ?');
 	$id=\mt_rand(1, $max);
-	return $st->execute(['aaa',$id]);
+	return executeQuery($db,"update user_ set lastname='aaa' where id='$id'");
 }

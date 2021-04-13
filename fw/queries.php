77c479ebf56ad428f40cdf6d97dde08c190ff5ed
J-C HERON <myaddressmail@gmail.com>
2021-04-13 11:41:15 +0200

    return to queries-old

--- fw/queries.php
+++ fw/queries.php
@@ -9,11 +9,9 @@ const OPS=[OP_SELECT_PK,OP_SELECT_NO_PK,OP_JOIN,OP_UPDATE,OP_LIKE,OP_COUNT];
 
 function connect($dsn,$user,$password){
 	try {
-		$db = new \PDO('pgsql:host=127.0.0.1;dbname=pgsqllarge', $user, $password,
-		[
-			\PDO::ATTR_PERSISTENT => true,
-			\PDO::ATTR_EMULATE_PREPARES => false
-		]);
+		$db = new \PDO('pgsql:host=127.0.0.1;dbname=pgsqllarge', $user, $password);
+		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
 	} catch (\PDOException $e) {
 		print "Error!: " . $e->getMessage() . "<br/>";
 		return false;
@@ -26,48 +24,33 @@ function query(\PDO $db,string $sql){
 	return $st->fetchAll();
 }
 
-function prepare(\PDO $db,$sql):\PDOStatement{
-	return $db->prepare($sql);
-}
-
 function executeQuery(\PDO $db,string $sql){
-	$st=prepare($db,$sql);
-	return $st->execute();
+	return $db->exec($sql);
 }
 
 function selectPkQuery(\PDO $db,int $max){
-	$st=prepare($db,'select * from user_ where id= ?');
 	$id=\mt_rand(1, $max);
-	$st->execute([$id]);
-	return $st->fetchAll();
+	return query($db,"select * from user_ where id='$id'");
 }
 
 function selectNoPKQuery(\PDO $db){
-	$st=prepare($db,'select * from user_ where firstname= ?');
-	$st->execute(['Winter']);
-	return $st->fetchAll();
+	return query($db,"select * from user_ where firstname='Winter'");
 }
 
 function joinQuery(\PDO $db){
-	$st=prepare($db,'select category_.* from user_ inner join category_ on user_.idCategory=category_.id where firstname= ?');
-	$st->execute(['Winter']);
-	return $st->fetchAll();
+	return query($db,"select category_.* from user_ inner join category_ on user_.idCategory=category_.id where firstname='Winter'");
 }
 
 function updateQuery(\PDO $db,int $max){
-	$st=prepare($db,'update user_ set lastname= ? where id= ?');
 	$id=\mt_rand(1, $max);
-	return $st->execute(['aaa',$id]);
+	return executeQuery($db,"update user_ set lastname='aaa' where id='$id'");
 }
 
 function likeQuery(\PDO $db){
-	$st=prepare($db,'select * from user_ where firstname like ?');
-	$st->execute(['%Winter%']);
-	return $st->fetchAll();
+	return query($db,'select * from user_ where firstname like "%Winter%"');
 }
 
 function countQuery(\PDO $db){
-	$st=prepare($db,'select count(*) from user_');
-	$st->execute();
-	return $st->fetchAll();
+	return query($db,'select count(*) from user_');
+
 }

16134949a9d21d255434b8956303258a17fd5717
jcheron <myaddressmail@gmail.com>
2021-05-06 20:24:26 +0200

    update U config

--- fw/ubiquity/app/config/config.php
+++ fw/ubiquity/app/config/config.php
@@ -11,19 +11,11 @@ return array(
 				"options"=>[],
 				"cache"=>false
 		],
-		"sessionName"=>"ubiquity",
+		"sessionName"=>null,
 		"namespaces"=>[],
-		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
 		"templateEngineOptions"=>array("cache"=>false),
 		"test"=>false,
 		"debug"=>false,
-		"logger"=>function(){return new \Ubiquity\log\libraries\UMonolog("ubiquity",\Monolog\Logger::INFO);},
-		"di"=>["@exec"=>["jquery"=>function($controller){
-						return \Ubiquity\core\Framework::diSemantic($controller);
-					}]],
 		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
-		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""],
-		"isRest"=>function(){
-			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
-		}
+		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""]
 );

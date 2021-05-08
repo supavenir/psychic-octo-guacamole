67dc9d23f2a4a1232cd70f054da109ae1f762fe8
jcheron <myaddressmail@gmail.com>
2021-05-08 18:07:39 +0200

    add db queries

--- benchmarks/db_queries.php
+++ benchmarks/db_queries.php
@@ -1,24 +1,25 @@
 <?php
-/*
-$dsn = 'mysql:dbname=myisam-small;host=127.0.0.1';
-$user = 'sio2a';
-$password = 'sio2a';
+if(isset($_GET['queries'])) {
+	$dsn = 'mysql:dbname=innodb-large;host=127.0.0.1';
+	$user = 'sio2a';
+	$password = 'sio2a';
 
-try {
-	$db = new PDO($dsn, $user, $password);
-	$clear = @$argv[1];
-	$st=$db->query("Show session status like 'Queries';");
-	$q= $st->fetchColumn(1);
-	$st->closeCursor();
-	$st=$db->query("show session status like 'Innodb_rows_read';");
-	$r= $st->fetchColumn(1);
-	echo $q.':'.$r;
-	$st->closeCursor();
-	if(isset($clear)){
-		$db->exec("RESET QUERY CACHE;");
+	try {
+		$db = new \PDO($dsn, $user, $password);
+		$clear = @$argv[1];
+		$st = $db->query("Show session status like 'Queries';");
+		$q = $st->fetchColumn(1);
+		$st->closeCursor();
+		$st = $db->query("show session status like 'Innodb_rows_read';");
+		$r = $st->fetchColumn(1);
+		echo $q . ':' . $r;
+		$st->closeCursor();
+		if (isset($clear)) {
+			$db->exec("RESET QUERY CACHE;");
+		}
+	} catch (\PDOException $e) {
+		echo $e->getMessage();
 	}
-} catch (PDOException $e) {
-	echo $e->getMessage();
-}
-*/
+}else {
 	echo '0:0';
+}
